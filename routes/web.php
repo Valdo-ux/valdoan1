e8ec091e50cd3a64757e307d777914cb2c00b071
minniverse <miny37088@gmail.com>
2025-03-15 14:17:12 +0700

    Menambahkan fitur login (controller, view, dan route)

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,30 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
 });
+
+Route ::get('/user/{id}', function ($id){
+    return 'User Dengan ID ' .$id;
+});
+
+Route::prefix('admin')->group(function (){
+    Route::get('dashboard', function (){
+        return 'Admin Dashboard';
+    });
+
+    Route::get('/users', function () {
+        return 'Admin Users';
+    });
+});
+
+Route::get('/listbarang/{id}/{nama}', function($id, $nama){
+  return view('list_barang', compact('id', 'nama'));
+
+});
+
+Route::get('/list_barang/{id}/{nama}', [ListBarangController::class, 'tampilkan']);
+
+use App\Http\Controllers\LoginController;
+Route::get('/login', [LoginController::class, 'index']);
